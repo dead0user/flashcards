e136c158a992be7c779284bbb279962aa0b02878
Marcin Adamski <dead0user@gmail.com>
2023-08-27 22:48:57 +0200

    Działające pobieranie słówek z pliku

--- app.py
+++ app.py
@@ -4,23 +4,39 @@ from os import system
 
 # TODO: dodać zaciąganie słowek z pliku
 # dwuwymiarowa tablica ze słówkami
-cards = [
-    ["accident", "wypadek"],
-    ["across", "przez"],
-    ["adventure", "przygoda"],
-    ["advice", "rada"],
-    ["almost", "prawie"],
-    ["alone", "sam"],
-    ["along", "wzdłóż"],
-    ["among", "pośród"],
-    ["arrive", "przybyć"],
-    ["become", "stać się"],
-    ["belong", "należeć"],
-    ["bill", "rachunek"],
-    ["blanket", "koc"],
-    ["bowl", "miska"],
-    ["broken", "złamany"]
-]
+file_with_words = "words.txt"
+# cards = [
+#     ["accident", "wypadek"],
+#     ["across", "przez"],
+#     ["adventure", "przygoda"],
+#     ["advice", "rada"],
+#     ["almost", "prawie"],
+#     ["alone", "sam"],
+#     ["along", "wzdłóż"],
+#     ["among", "pośród"],
+#     ["arrive", "przybyć"],
+#     ["become", "stać się"],
+#     ["belong", "należeć"],
+#     ["bill", "rachunek"],
+#     ["blanket", "koc"],
+#     ["bowl", "miska"],
+#     ["broken", "złamany"]
+# ]
+
+
+def file_to_list(filename):
+    file_name = filename
+    words = open(file_name, "r")
+    words_list = words.readlines()
+    cards = []
+    for line in words_list:
+        nline = line.replace("\n", "")
+        pair = nline.split(";")
+        cards.append(pair)
+    return cards
+
+# lista z parami słówek
+cards = file_to_list(file_with_words)
 
 # zmienne zliczania odpowiedzi
 correct = 0
